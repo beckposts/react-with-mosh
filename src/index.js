f08b820daf692f85003ab64e3715d5ce2838435e
doordion <doordion@gmail.com>
2022-07-09 11:25:41 +0530

    commenting basics

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 // React is an object and react is the module
 import ReactDOM from 'react-dom';
 // ReactDOM is an object importing from 'react-dom' module
+import bootstrap from 'bootstrap/dist/css/bootstrap.css';
+// const element = <h1>Hello World!</h1>;
+// console.log(element);
+// element is the part of the virtual DOM
 
-const element = <h1>Hello World!</h1>;
-console.log(element); // element is the part of the virtual DOM
 /* 
     Now render this element inside real DOM
     That's why we've imported ReactDOM
     We have imported ReactDOM to render our JSX
  */
 
-ReactDOM.render(element, document.getElementById('root'));
-// First arg: element we want to render
-// Second arg: Where we want to render
+// ReactDOM.render(element, document.getElementById('root'));
+
+/* 
+    First arg: element we want to render
+    Second arg: Where we want to render
+ */
