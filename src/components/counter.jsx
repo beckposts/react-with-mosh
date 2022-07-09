696aff2119e3105566b2386a6d3d7535f2474eb8
doordion <doordion@gmail.com>
2022-07-09 11:37:29 +0530

    created first component

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'; // imrc shorcut
+
+// generate class shortcut: cc
+class Counter extends Component {
+  render() {
+    return <h1>Hello World </h1>;
+  }
+}
+
+export default Counter;
