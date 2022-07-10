867367aa86ca5dd63e9f8dcb466a5127e651ec3f
doordion <doordion@gmail.com>
2022-07-10 14:03:15 +0530

    Rendering classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,17 +3,27 @@ import React, { Component } from 'react'; // imrc shorcut
 // generate class shortcut: cc
 class Counter extends Component {
   state = {
-    count: 2,
-    imageUrl: 'https://picsum.photos/200',
+    count: 0,
   };
+
+  styles = {
+    fontSize: '30px',
+    fontWeight: 'bold',
+  };
+
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imageUrl} />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className='btn btn-secondary btn-sm'>Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCount() {
